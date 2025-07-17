ff442afba4bb1afbd19fe4da0052409d4297cb06
Darlenne Noemi Mosqueda <dnmosquedaa@uncg.edu>
2025-07-17 13:09:51 -0400

    initial commit

---
+++ IsraelsWeb/src/main/java/com/example/IsraelsWeb/IsraelsWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.IsraelsWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsraelsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsraelsWebApplication.class, args);
+	}
+
+}
---
+++ IsraelsWeb/src/test/java/com/example/IsraelsWeb/IsraelsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IsraelsWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsraelsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
